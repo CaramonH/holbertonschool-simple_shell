f2d6d53296335ba4eba913719891631968128f7c
CaramonH <caramonh@yahoo.com>
2023-04-07 09:18:36 -0700

    list ops

--- main.h
+++ main.h
@@ -4,5 +4,26 @@
 /* Standard Libraries */
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string.h>
+#include <stddef.h>
+#include <sys/wait.h>
+#include <limits.h>
+#include <signal.h>
+
+
+/**
+ * struct list_s - singly linked list
+ * @str: string
+ * @next: next node pointer
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+	char *str;
+	struct listy_s *next;
+} list_t;
 
 #endif
---
+++ shell.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * main - runs simple shell
+ * @
+ * @
+ * @
+ *
+ * Return: 0 on success
+ */
+
