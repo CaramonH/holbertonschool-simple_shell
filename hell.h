407007b7fe299cf7bf3d79ef97dc30d7023dfbd6
CaramonH <caramonh@yahoo.com>
2023-04-11 11:26:16 -0700

    merge

--- hell.h
+++ hell.h
@@ -9,23 +9,7 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-/**
- * struct list_s - singly linked list
- * @str: string - malloc'd string
- * @next: pointer to the next node
- *
- * Description:: singly linked list node structure
- */
-typedef struct list_s
-{
-	char *str
-		struct list_s *next;
-} list_t;
-
 char **split_string(char *str, int *count);
 int main(void);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-list_t *insert_node_at_index(list_t **head, unsigned int idx, char *str);
 
 #endif
